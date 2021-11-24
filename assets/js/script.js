0ec0574e551a62bf82e1787bbadac122d7a25d46
hamid <hbeigi57@gmail.com>
2021-11-24 13:36:04 +0330

    about us & our services & best works compelete

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    lightbox.init();
+    lightbox.option({
+        'resizeDuration': 200,
+        'wrapAround': true,
+        alwaysShowNavOnTouchDevices :true , 
+        albumLabel : " کار برتر %1از %2",
+        maxWidth : 700
+      })
+});
